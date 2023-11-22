4d301cbcb18cbc08cb4620fae9cd6a9c8fe1ba05
charlottexlin02@gmail.com <charlottexlin02@gmail.com>
2023-11-22 17:23:14 -0500

    checklist UI

---
+++ Assets/Scripts/Checklist.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checklist : MonoBehaviour
+{
+    private GameObject[] checkmarks = new GameObject[10];
+
+    // Mapping of item name to checkmark index
+    private Dictionary<string, int> checkmarkIndices = new() {
+        {"Postage stamp", 0},
+        {"Thimble", 1},
+        {"Button", 2},
+        {"Acorn cap", 3},
+        {"Christmas light", 4},
+        {"Postcard", 5},
+        {"Feather", 6},
+        {"Pocket watch", 7},
+        {"Bottle cap", 8},
+        {"Seashell", 9},
+    };
+    
+    private void Start() {
+        // Populate `checkmarks` array with of this game object's children
+        int i = 0;
+        foreach (Transform child in transform) {
+            checkmarks[i] = child.gameObject;
+            i++;
+        }
+    }
+
+    private void OnEnable() {
+        foreach (var (itemName, index) in checkmarkIndices) {
+            // This item has been collected, so enable its checkmark
+            if (ItemsToCollect.items[itemName]) {
+                checkmarks[index].SetActive(true);
+            }
+        }
+    }
+}
